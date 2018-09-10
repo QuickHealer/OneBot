7cdfbbd97ee1169debc324db2960d56d45b7b6b5
QuickHealer <coryhealey@hotmail.com>
2018-09-10 14:45:50 -0700

    Bot built using https://blog.botframework.com/2018/05/07/build-a-microsoft-bot-framework-bot-with-the-bot-builder-sdk-v4/

---
+++ WebApplication1/MyBot.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class MyBot : IBot
+    {
+        public async Task OnTurn(ITurnContext turnContext)
+        {
+            if (turnContext.Activity.Type is ActivityTypes.Message)
+            {
+                string userInput = turnContext.Activity.Text;
+                await turnContext.SendActivity($"You wrote {userInput}");
+            }
+        }
+    }
+}
